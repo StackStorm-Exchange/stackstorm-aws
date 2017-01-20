7739bb09d258828e5ce36697b179fe9d487c9ec7
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-01-20 02:35:26 +0000

    corrected the test for CloudFormation

--- tests/test_action_cloudformation.py
+++ tests/test_action_cloudformation.py
@@ -1,18 +1,27 @@
 import mock
 
+from run import ActionManager
 from aws_base_action_test_case import AWSBaseActionTestCase
 
 from boto.cloudformation.connection import CloudFormationConnection
 
 
 class CloudFormationTestCase(AWSBaseActionTestCase):
-    action_cls = CloudFormationConnection
+    __test__ = True
+    action_cls = ActionManager
+
+    def setUp(self):
+        super(CloudFormationTestCase, self).setUp()
+        self._params = {
+            'module_path': 'boto.cloudformation.connection',
+            'cls': 'CloudFormationConnection',
+        }
 
     @mock.patch.object(CloudFormationConnection, 'get_path', mock.Mock(return_value='hoge'))
-    def test_connect(self):
+    def test_connection(self):
         self._params['action'] = 'get_path'
 
         action = self.get_action_instance(self.full_config)
         result = action.run(**self._params)
 
-        self.assertEqual(result, 'hoge')
+        self.assertEqual(result, ['hoge'])
