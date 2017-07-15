9bea2b370882b4b757962da31685f4ffba66c444
Dinesh Weerapurage <dinesh.weerapurage@pearson.com>
2017-07-15 10:03:57 -0600

    Using datetime serialization in assume_role for boto3action

--- actions/assume_role.py
+++ actions/assume_role.py
@@ -1,11 +1,24 @@
+from datetime import date, datetime
+import json
+
 import boto3
 
 from st2actions.runners.pythonrunner import Action
 
 
 # pylint: disable=too-few-public-methods
+def json_serial(obj):
+    if isinstance(obj, (datetime, date)):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+# pylint: disable=too-few-public-methods
 class Boto3AssumeRoleRunner(Action):
-    def run(self, role_arn, role_session_name, policy, duration, external_id, serial_number, token_code):
+    def run(
+            self, role_arn, role_session_name, policy,
+            duration, external_id, serial_number, token_code):
         client = boto3.client('sts')
         kwargs = {}
         kwargs['RoleArn'] = role_arn
@@ -13,7 +26,7 @@ class Boto3AssumeRoleRunner(Action):
         kwargs['DurationSeconds'] = duration
         if policy is not None:
             kwargs['Policy'] = policy
-            
+
         if external_id is not None:
             kwargs['ExternalId'] = external_id
 
@@ -23,6 +36,6 @@ class Boto3AssumeRoleRunner(Action):
         if token_code is not None:
             kwargs['TokenCode'] = token_code
 
-	response = client.assume_role(**kwargs)
-	response['Credentials']['Expiration'] = response['Credentials']['Expiration'].isoformat()
+        response = client.assume_role(**kwargs)
+        response = json.loads(json.dumps(response, default=json_serial))
         return (True, response)
