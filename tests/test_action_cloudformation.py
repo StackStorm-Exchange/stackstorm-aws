82276aa38e226e8935ac722de50ad2d33e743b25
Stefan Gusa <gusa@adobe.com>
2019-10-14 17:20:50 +0300

    Multiaccount action capability

--- tests/test_action_cloudformation.py
+++ tests/test_action_cloudformation.py
@@ -1,9 +1,11 @@
 import mock
 
 from run import ActionManager
+from boto3.session import Session
 from aws_base_action_test_case import AWSBaseActionTestCase
 
 from boto.cloudformation.connection import CloudFormationConnection
+from botocore.exceptions import ClientError
 
 
 class CloudFormationTestCase(AWSBaseActionTestCase):
@@ -17,11 +19,48 @@ class CloudFormationTestCase(AWSBaseActionTestCase):
             'cls': 'CloudFormationConnection',
         }
 
+    @mock.patch.object(Session, 'client',
+                       mock.Mock(return_value=AWSBaseActionTestCase.MockStsClient()))
     @mock.patch.object(CloudFormationConnection, 'get_path', mock.Mock(return_value='hoge'))
-    def test_connection(self):
+    def _connection(self, config, additional_params):
         self._params['action'] = 'get_path'
+        self._params.update(additional_params)
 
-        action = self.get_action_instance(self.full_config)
+        action = self.get_action_instance(config)
         result = action.run(**self._params)
 
         self.assertEqual(result, ['hoge'])
+
+    def test_connection_full_config(self):
+        self._connection(self.full_config, {})
+
+    def test_connection_multiaccount_config(self):
+        params = {
+            'account_id': '345678901223',
+            'region': 'us-east-1'
+        }
+        self._connection(self.multiaccount_config, params)
+
+    @mock.patch.object(Session, 'client', mock.Mock(
+        return_value=AWSBaseActionTestCase.MockStsClientRaiseClientError()))
+    @mock.patch.object(CloudFormationConnection, 'get_path', mock.Mock(return_value='hoge'))
+    def test_fails_assuming_role(self):
+        self._params.update({
+            'action': 'get_path',
+            'account_id': '345678901223',
+            'region': 'us-east-1'
+        })
+        action = self.get_action_instance(self.multiaccount_config)
+        self.assertRaises(ClientError, lambda: action.run(**self._params))
+
+    @mock.patch.object(Session, 'client',
+                       mock.Mock(return_value=AWSBaseActionTestCase.MockStsClient()))
+    @mock.patch.object(CloudFormationConnection, 'get_path', mock.Mock(return_value='hoge'))
+    def test_fails_with_missing_arn_multiaccount_config(self):
+        self._params.update({
+            'action': 'get_path',
+            'account_id': '345678901223',
+            'region': 'us-east-1'
+        })
+        action = self.get_action_instance(self.full_config)
+        self.assertRaises(KeyError, lambda: action.run(**self._params))
